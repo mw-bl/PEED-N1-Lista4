928cfe3e63536530c23ee763855732cb6d1c09c5
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2023-06-09 16:57:09 -0300

    Update Q11.py
    Questão corrigida, acabei inserindo outra questão de outro exercício que havia feito.

--- Q11.py
+++ Q11.py
@@ -1,13 +1,68 @@
+#Questão corrigida, acabei inserindo outra questão de outro exercício que havia feito.
+class No:
+    def __init__(self, dado):
+        self.dado = dado
+        self.ant = None
+        self.prox = None
+
+
+class ListaEncadeadaDupla:
+    def __init__(self):
+        self.primeiro = None
+        self.ultimo = None
+
+    def estaVazia(self):
+        return self.primeiro is None
+
+    def inserirEmOrdem(self, dado):
+        novoNo = No(dado)
+
+        if self.estaVazia():
+            self.primeiro = novoNo
+            self.ultimo = novoNo
+        elif dado < self.primeiro.dado:
+            novoNo.prox = self.primeiro
+            self.primeiro.ant = novoNo
+            self.primeiro = novoNo
+        elif dado > self.ultimo.dado:
+            novoNo.ant = self.ultimo
+            self.ultimo.prox = novoNo
+            self.ultimo = novoNo
+        else:
+            atual = self.primeiro
+            while atual is not None and dado > atual.dado:
+                atual = atual.prox
+
+            novoNo.ant = atual.ant
+            novoNo.prox = atual
+            atual.ant.prox = novoNo
+            atual.ant = novoNo
+
+    def exibirLista(self):
+        if self.estaVazia():
+            print("A lista está vazia.")
+            return
+
+        atual = self.primeiro
+        while atual is not None:
+            print(atual.dado, end=" ")
+            atual = atual.prox
+        print()
+
+
 def criar_lista_nomes():
-    nomes = [] 
+    lista = ListaEncadeadaDupla()
 
     while True:
-        nome = input("Digite um nome (ou 'sair' para encerrar e exibir a lista): ")
+        nome = input("Digite um nome (ou 'sair' para encerrar): ")
         if nome.lower() == "sair":
-            break  
-        nomes.append(nome) 
+            break
+        lista.inserirEmOrdem(nome)
 
-    return nomes
+    return lista
+
+
 
 lista_nomes = criar_lista_nomes()
-print("Lista de nomes:", lista_nomes)
+print("Lista de nomes em ordem alfabética:")
+lista_nomes.exibirLista()
